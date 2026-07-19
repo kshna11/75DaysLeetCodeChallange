13120d87177327f69cbc5d7f8e0ba1aae2ea5335
kshna11 <28krishna.j@gmail.com>
2026-07-19 13:40:20 +0530

    Time: 0 ms (100%), Space: 11.9 MB (8.76%) - LeetHub

---
+++ 0942-di-string-match/0942-di-string-match.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> diStringMatch(string s) {
+        int low = 0, high = s.length();
+        vector<int> ans;
+
+        for (char ch : s) {
+            if (ch == 'I')
+                ans.push_back(low++);
+            else
+                ans.push_back(high--);
+        }
+
+        ans.push_back(low);
+
+        return ans;
+    }
+};
