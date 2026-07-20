1f2d76ba06f5e344be3275762e534c86a3992208
kshna11 <28krishna.j@gmail.com>
2026-07-20 20:35:28 +0530

    Time: 0 ms (100%), Space: 8.4 MB (39.47%) - LeetHub

--- 1974-minimum-time-to-type-word-using-special-typewriter/1974-minimum-time-to-type-word-using-special-typewriter.cpp
+++ 1974-minimum-time-to-type-word-using-special-typewriter/1974-minimum-time-to-type-word-using-special-typewriter.cpp
@@ -1,21 +1,15 @@
 class Solution {
 public:
     int minTimeToType(string word) {
-
         char current = 'a';
         int time = 0;
-
         for(char ch : word){
-
             int diff = abs(ch-current);
-                                        
             time += min(diff,26-diff);
-
             time++;
 
             current = ch;
         }
-
         return time;
     }
 };
